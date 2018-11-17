dfbecea1cf5d615f0220fd278c32b3dae7b77067
Mayank Nader <mayank.nader11@gmail.com>
2018-11-17 14:37:23 +0530

    jpeg image permission

--- color/forms.py
+++ color/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import NumberRange
 
 
 class PhotoForm(FlaskForm):
-    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
     palette_height = IntegerField('Palette Height', validators=[NumberRange(1, 10)])
     palette_outline_width = IntegerField('Palette Outline Width', validators=[NumberRange(1, 40)])
 
