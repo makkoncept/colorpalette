f0a504d1d9fb959631d53eb293579446e4ba8edf
Mayank Nader <mayank.nader11@gmail.com>
2020-09-23 22:31:48 +0530

    Update project config
    - Decrease image size upload limit from 4MB to 2MB
    - randomize the fallback secret key

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "abadsecretkey"
-    MAX_CONTENT_LENGTH = 4 * 1024 * 1024  # image size upload limit (4 MB)
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "N6q067SQG7894Yw28d85BqWu4"
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # image size upload limit (2 MB)
